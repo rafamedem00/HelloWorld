801f1f8d07f5895d1f703f996755c427c3bc5c5e
rafa <rafamedem@hotmail.com>
2019-09-17 12:00:25 +0200

    second version

---
+++ Ses4Ex2.py
@@ -0,0 +1,8 @@
+print("Please enter two numbers separated by comma: ")
+num1 = input("1st number")
+num2 = input("2nd number")
+num1 = int(num1)
+num2 = int(num2)
+result = num1/num2
+result = int(result+0.5)
+print("The rounded up result is " + str(result))
---
+++ Ses4Ex3.py
@@ -0,0 +1,5 @@
+import math
+r=input("To calculate te surface of a circle, please enter the radius in cm: ")
+n=int(r)
+r=(n**2)*math.pi
+print("The surface of the circle is " + str(r) + " cm^2")
